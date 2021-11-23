d3616396e2c4757844132db647cc3fb0e3f4f82a
Cris <cristiandcrane@gmail.com>
2021-11-23 02:22:21 -0500

    add yup and formik, implement contact form

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
+import Input from "./forms/Input";
 
 export default function Footer() {
   return (
@@ -57,61 +60,41 @@ const InfoArea = () => {
 const ContactForm = () => {
   return (
     <section className="section">
-      <form>
-        <h2 className="title has-text-grey-dark is-size-4">Contact</h2>
-        <div className="field">
-          <label className="label is-small">Name</label>
-          <div className="control has-icons-left">
-            <input
-              className="input is-small"
-              type="text"
-              placeholder="Text input"
-              value="bulma"
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-user"></i>
-            </span>
-          </div>
-        </div>
-        <div className="field">
-          <label className="label is-small">Email</label>
-          <div className="control has-icons-left has-icons-right">
-            <input
-              className="input is-small"
-              type="email"
-              placeholder="Email input"
-              value="hello@"
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-envelope"></i>
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-exclamation-triangle"></i>
-            </span>
-          </div>
-          <p className="help is-danger">This email is invalid</p>
-        </div>
-        <div className="field">
-          <label className="label is-small">Subject</label>
-          <div className="control">
-            <div className="select is-small">
-              <select>
-                <option>Select dropdown</option>
-                <option>With options</option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <div className="field">
-          <label className="label is-small">Message</label>
-          <div className="control">
-            <textarea
-              className="textarea is-small"
-              placeholder="Textarea"
-            ></textarea>
-          </div>
-        </div>
-      </form>
+      <h2 className="title has-text-grey-dark is-size-4">Contact</h2>
+      <Formik
+        initialValues={{
+          fullName: "",
+          email: "",
+        }}
+        validationSchema={Yup.object({
+          fullName: Yup.string().required("Required"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }}
+      >
+        <Form>
+          <Input
+            label="Full Name"
+            name="fullName"
+            type="text"
+            leftIcon={<i className="fas fa-user"></i>}
+            size="small"
+          />
+          <Input
+            label="Email"
+            name="email"
+            type="email"
+            leftIcon={<i className="fas fa-envelope"></i>}
+            size="small"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </section>
   );
 };
